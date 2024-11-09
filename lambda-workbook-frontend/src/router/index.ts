967d8a81e299c0e3301bdfaff6b949edb670b5d6
Valery Dzeviatkin <valera9kin@inbox.ru>
2024-11-09 21:26:32 +0300

    Kickoff

---
+++ lambda-workbook-frontend/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/page1',
+      name: 'page1',
+      component: () => import('../views/Page1View.vue'),
+    },
+    {
+      path: '/page2',
+      name: 'page2',
+      component: () => import('../views/Page2View.vue'),
+    },
+    {
+      path: '/page3',
+      name: 'page3',
+      component: () => import('../views/Page3View.vue'),
+    },
+    {
+      path: '/page4',
+      name: 'page4',
+      component: () => import('../views/Page4View.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ lambda-workbook-frontend/src/views/Page1View.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>This is Page 1</h1>
+</template>
+
+<style>
+</style>
