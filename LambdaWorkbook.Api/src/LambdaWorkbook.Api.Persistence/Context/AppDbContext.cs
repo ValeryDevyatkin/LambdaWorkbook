341401752460e72bff3afa4ef6cd9ff635a953df
Valery Dzeviatkin <valera9kin@inbox.ru>
2024-11-17 13:19:38 +0300

    Update Create timestamp applied during model save

--- LambdaWorkbook.Api/src/LambdaWorkbook.Api.Persistence/Context/AppDbContext.cs
+++ LambdaWorkbook.Api/src/LambdaWorkbook.Api.Persistence/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using LambdaWorkbook.Api.Domain.Model;
+using LambdaWorkbook.Api.Domain.Model.Base;
 using Microsoft.EntityFrameworkCore;
 
 namespace LambdaWorkbook.Api.Persistence.Context;
@@ -12,4 +13,27 @@ public class AppDbContext : DbContext
     public DbSet<IdentityUser> IdentityUsers { get; set; }
     public DbSet<IdentityRole> IdentityRoles { get; set; }
     public DbSet<UserNote> UserNotes { get; set; }
+
+    public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries()
+            .Where(e => e.Entity is ModelBase &&
+                (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+        var updateTime = DateTime.UtcNow;
+
+        foreach (var entry in entries)
+        {
+            var trackableEntity = (ModelBase)entry.Entity;
+
+            if (entry.State == EntityState.Added)
+            {
+                trackableEntity.CreatedAt = updateTime;
+            }
+
+            trackableEntity.ModifiedAt = updateTime;
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
 }
