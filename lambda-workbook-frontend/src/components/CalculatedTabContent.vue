779d53a7c13dabfd8a17bcde7b9f8e4680f706f4
Valery Dzeviatkin <valera9kin@inbox.ru>
2024-11-25 17:42:02 +0300

    Height calculation improved for page reload

--- lambda-workbook-frontend/src/components/CalculatedTabContent.vue
+++ lambda-workbook-frontend/src/components/CalculatedTabContent.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/store/auth-store'
 import { useHeightNormalizerStore } from '@/store/height-normalizer-store'
 
 const heightStore = useHeightNormalizerStore()
+const authStore = useAuthStore()
 </script>
 
 <template>
-  <div v-if="heightStore.isCalculating" class="tab-content" id="tab-content-template">
+  <div
+    v-if="heightStore.isCalculating && authStore.isAuthorized"
+    class="tab-content"
+    id="tab-content-template"
+  >
     <div class="center-message">Загрузка...</div>
   </div>
   <div
